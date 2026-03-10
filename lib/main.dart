a94e84978eeaf7e9789f228834a64c1542711653
Akbarre <akbarzainalapp@gmail.com>
2026-03-11 01:12:49 +0700

    update img and fix eroor and update nim

--- lib/main.dart
+++ lib/main.dart
@@ -38,8 +38,46 @@ class CardPage extends StatelessWidget {
               ),
             ],
           ),
-          child: const Column(mainAxisSize: MainAxisSize.min, children: [
-              
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('lib/pp.jpg'),
+              ),
+
+              const SizedBox(height: 20),
+
+              const Text(
+                'M Zainal Akbar',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+
+              const Text(
+                'Mahasiswa IT - Semester 4',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+
+              const Divider(
+                height: 40,
+                thickness: 1,
+                indent: 20,
+                endIndent: 20,
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Icon(Icons.email, color: Colors.blueGrey),
+                  ),
+                  Text(
+                    'akbarzainalapp166@gmail.com',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
